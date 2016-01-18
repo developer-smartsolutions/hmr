510c58454909aab339869a649fd6ee099ea9308d
Rucha <ruch.deshmukh@gmail.com>
2016-01-17 17:18:05 -0800

    initial commit

---
+++ app/src/main/java/smartsolutions/hmr/HMRFragment.java
@@ -0,0 +1,29 @@
+package smartsolutions.hmr;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ruchadeshmukh on 1/17/16.
+ */
+public class HMRFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance){
+        super.onActivityCreated(savedInstance);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
